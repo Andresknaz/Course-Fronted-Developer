0d4624a18e8e3a87200ee7795c2796f116461e15
andresknaz <andrescanaveral.p@gmail.com>
2025-03-25 22:15:32 -0500

    Add data type

---
+++ tipo-de-datos.js
@@ -0,0 +1,42 @@
+//PRIMITIVOS
+
+//String
+let nombre = 'Andres'
+
+//Number
+let edad = 26
+
+//Boolean
+
+let esMayorDeEdad = true
+
+// null
+let noHayValor = null
+
+//undefined
+let noDefinido = undefined
+
+// symbol
+let simboloUnico = Symbol('unico')
+
+//bigint
+
+let numeroGrande = 2n
+
+// COMPLEJOS
+
+//Object
+
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+
+//array
+
+let frutas = ['manzana','banano','uvas']
+
+//function
+function saludar (){
+    
+}
