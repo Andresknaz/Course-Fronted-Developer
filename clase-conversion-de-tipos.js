d6edf993b6ec767045c6e0740adccb3af5a4f4dd
andresknaz <andrescanaveral.p@gmail.com>
2025-04-06 16:44:51 -0500

    Update explicit type casting

--- clase-conversion-de-tipos.js
+++ clase-conversion-de-tipos.js
@@ -9,3 +9,33 @@ const stringDecimal = '3.14'
 const float = parseFloat(stringDecimal)
 console.log(float)
 console.log(typeof float)
+
+const binary = '1010'
+const decimal = parseInt(binary, 2)
+console.log(decimal)
+console.log(typeof decimal)
+
+//Implicit Type Casting
+
+const sum = '5' + 3
+console.log(sum)
+
+const sumWithBoolean = '3' + true
+console.log(sumWithBoolean)
+
+const sumWithNumber = 2 + true
+console.log(sumWithNumber)
+
+const stringValue = '10'
+const numberValue = 10
+const booleanValue = true
+console.log('.................')
+console.log(stringValue + stringValue) //CONCATENA
+console.log(stringValue + numberValue)
+console.log(stringValue + booleanValue)
+console.log(numberValue + stringValue) 
+console.log(numberValue + numberValue) //SUMA
+console.log(numberValue + booleanValue) //SUMA
+console.log(booleanValue + stringValue) 
+console.log(booleanValue + numberValue) //SUMA
+console.log(booleanValue + booleanValue) //SUMA
