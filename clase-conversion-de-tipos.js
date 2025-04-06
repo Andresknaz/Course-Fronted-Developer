6f8564648e6aedc8b4936e709d394110207fdcba
andresknaz <andrescanaveral.p@gmail.com>
2025-04-06 16:31:47 -0500

    Implementeed explicit type casting

---
+++ clase-conversion-de-tipos.js
@@ -0,0 +1,11 @@
+//Explicit Type Casting
+
+const string = '42'
+const integer = parseInt(string)
+console.log(integer)
+console.log(typeof integer)
+
+const stringDecimal = '3.14'
+const float = parseFloat(stringDecimal)
+console.log(float)
+console.log(typeof float)
